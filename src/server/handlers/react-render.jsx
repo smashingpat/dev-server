c571adfc1961d8a73b793d9908b3e15bffb5824e
smashingpat <patt.gerr@gmail.com>
2017-08-11 15:01:24 +0200

    added server refresh & eventemitter

--- src/server/handlers/react-render.jsx
+++ src/server/handlers/react-render.jsx
@@ -4,11 +4,22 @@ import App from '../../shared/App';
 import manifest from '../manifest';
 
 const createHtml = render => `
-    <link rel="stylesheet" href="${manifest.main.css}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    ${manifest.main.css ? `<link rel="stylesheet" href="${manifest.main.css}">` : ''}
+</head>
+<body>
     <div id="mount">${render}</div>
     <script src="${manifest.runtime.js}"></script>
     <script src="${manifest.vendor.js}"></script>
     <script src="${manifest.main.js}"></script>
+</body>
+</html>
 `;
 
 export default (req, res) => {
